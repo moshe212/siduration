5db6f1e17b925dbb1e67b743cb78e5c3e6f71be5
moshe212 <moshe212@gmail.com>
2024-03-27 00:55:55 +0200

    d

--- utils/dbFunc/createInvitedFromFile.js
+++ utils/dbFunc/createInvitedFromFile.js
@@ -3,20 +3,20 @@ const xlsx = require("xlsx");
 // const fetch = require("node-fetch");
 const axios = require("axios");
 
-const createInvitedFromFile = async ({ userID, excelFileUrl }) => {
+const createInvitedFromFile = async ({ userId, excelFileUrl }) => {
   const airtableApiKey = process.env.AIRTABLE_API_KEY;
   const airtableBaseId = process.env.AIRTABLE_BASE_ID;
   const airtableBase = new Airtable({ apiKey: airtableApiKey }).base(
     airtableBaseId
   );
 
-  const getEventIdForUser = async (userID) => {
+  const getEventIdForUser = async (userId) => {
     console.log("getEventIdForUser");
-    console.log("userID: " + userID);
+    console.log("userId: " + userId);
     const eventsTable = airtableBase("Events");
     const records = await eventsTable
       .select({
-        filterByFormula: `{UserID} = '${userID}'`,
+        filterByFormula: `{UserID} = '${userId}'`,
       })
       .firstPage();
 
@@ -24,7 +24,7 @@ const createInvitedFromFile = async ({ userID, excelFileUrl }) => {
     return records.length > 0 ? records[0].fields.EventID : null;
   };
 
-  const addRowToInvitedTable = async (rowData, eventId, userID, index) => {
+  const addRowToInvitedTable = async (rowData, eventId, userId, index) => {
     console.log("addRowToInvitedTable");
     const invitedTable = airtableBase("Invited");
     const airtableRowData = {
@@ -41,7 +41,7 @@ const createInvitedFromFile = async ({ userID, excelFileUrl }) => {
       // 'Actually_Arrived_Count': parseInt(rowData['ActuallyArrivedCount'], 10), // Example conversion, adjust 'ActuallyArrivedCount' as needed
       Notes: rowData["Notes"], // Assuming direct match, adjust if needed
       MsgLang: 1,
-      UserID: userID, // This comes from the parameter, no need to adjust
+      UserID: userId, // This comes from the parameter, no need to adjust
     };
 
     // Filter out undefined values since Airtable API doesn't accept them
@@ -52,7 +52,7 @@ const createInvitedFromFile = async ({ userID, excelFileUrl }) => {
     await invitedTable.create(airtableRowData);
   };
 
-  const processExcelFile = async (url, eventId, userID) => {
+  const processExcelFile = async (url, eventId, userId) => {
     console.log("processExcelFile");
     const response = await axios.get(url, {
       responseType: "arraybuffer",
@@ -67,20 +67,20 @@ const createInvitedFromFile = async ({ userID, excelFileUrl }) => {
     for (const row of rows) {
       // Assuming row structure matches the Airtable "invited" table structure
       // You might need to adjust the mapping based on your Excel file structure
-      await addRowToInvitedTable(row, eventId, userID, index++);
+      await addRowToInvitedTable(row, eventId, userId, index++);
     }
   };
 
-  const addUserInvitesFromExcel = async (userID, excelFileUrl) => {
+  const addUserInvitesFromExcel = async (userId, excelFileUrl) => {
     console.log("addUserInvitesFromExcel");
     try {
-      const eventId = await getEventIdForUser(userID);
+      const eventId = await getEventIdForUser(userId);
       if (!eventId) {
         console.error("EventID not found for the given UserID");
         return;
       }
 
-      await processExcelFile(excelFileUrl, eventId, userID);
+      await processExcelFile(excelFileUrl, eventId, userId);
       console.log("All invites have been processed successfully.");
     } catch (error) {
       console.error("An error occurred:", error);
@@ -90,7 +90,7 @@ const createInvitedFromFile = async ({ userID, excelFileUrl }) => {
   // Example usage
   // const userId = 'yourUserIdHere';
   // const excelFileUrl = 'https://example.com/path/to/your/excel/file.xlsx';
-  addUserInvitesFromExcel(userID, excelFileUrl);
+  addUserInvitesFromExcel(userId, excelFileUrl);
 };
 
 module.exports = { createInvitedFromFile };
