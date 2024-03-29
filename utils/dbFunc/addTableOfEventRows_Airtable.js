f8f4d6a9d6c477d3e56ba8388cfb8db6d18b9ce1
moshe212 <moshe212@gmail.com>
2024-03-29 04:33:18 +0300

    ד

--- utils/dbFunc/addTableOfEventRows_Airtable.js
+++ utils/dbFunc/addTableOfEventRows_Airtable.js
@@ -57,7 +57,7 @@ const addTableOfEventRows_Airtable = async ({
           {
             id: eventRecordId,
             fields: {
-              IsTableAdded: true,
+              IsTableAdded: "true",
             },
           },
         ]);
