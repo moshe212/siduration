cb13560a6ab173578e4e82e0b6247d820780edd9
moshe212 <moshe212@gmail.com>
2024-04-03 10:16:07 +0300

    add basic endpoint for updateAmountSeats

--- index.js
+++ index.js
@@ -136,6 +136,34 @@ app.post("/api/createInvitedsFromFile", async (_req, res) => {
     });
 });
 
+app.post("/api/updateAmountSeats", async (_req, res) => {
+  console.log("createInvitedsFromFile");
+  console.log("req", _req.body);
+  const { eventID, tableID, amountSeats } = _req.body;
+  console.log(eventID, tableID, amountSeats);
+  // dbFunc
+  //   .createInvitedFromFile({
+  //     userId,
+  //     excelFileUrl,
+  //     eventIdFromAdalo: eventId,
+  //   })
+  //   .then((data) => {
+  //     // Assuming `data` is what the promise resolves with
+  //     console.log(`createInvitedFromFile: ${data}`);
+  //     // Successfully added row, send back a success response
+  //     res.status(200).send("ok");
+  //   })
+  //   .catch((error) => {
+  //     // Properly catch and handle any errors
+  //     console.error(error); // Log the error for debugging
+  //     res
+  //       .status(500)
+  //       .send(
+  //         "error on create createInvitedFromFile rows in Events table: " + error
+  //       );
+  //   });
+});
+
 app.get("/", (req, res) => {
   res.redirect("/he");
 });
