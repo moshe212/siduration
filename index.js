b4d2f6dbf82a47ec0a6ed00d015c6b5bbe51926b
moshe212 <moshe212@gmail.com>
2024-04-04 09:57:47 +0300

    add addInvitedToTable endpoint

--- index.js
+++ index.js
@@ -192,6 +192,31 @@ app.post("/api/deleteTable", async (_req, res) => {
     });
 });
 
+app.post("/api/addInvitedToTable", async (_req, res) => {
+  console.log("addInvitedToTable");
+  console.log("req", _req.body);
+  const { eventID, invitedID } = _req.body;
+  console.log(eventID, invitedID);
+  res.status(200).send("ok");
+  // dbFunc
+  //   .deleteTable({
+  //     eventID,
+  //     tableID,
+  //   })
+  //   .then((data) => {
+  //     // Assuming `data` is what the promise resolves with
+  //     console.log(`deleteTable: ${data}`);
+  //     // Successfully added row, send back a success response
+  //     res.status(200).send("ok");
+  //   })
+  //   .catch((error) => {
+  //     // Properly catch and handle any errors
+  //     console.error(error); // Log the error for debugging
+  //     res
+  //       .status(500)
+  //       .send("error on deleteTable rows in Events table: " + error);
+  //   });
+});
 app.get("/", (req, res) => {
   res.redirect("/he");
 });
