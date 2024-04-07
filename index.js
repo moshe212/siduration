73708c0ed415bfb9e7018060e0b3715a7d364d77
moshe212 <moshe212@gmail.com>
2024-04-07 16:23:33 +0300

    s

--- index.js
+++ index.js
@@ -282,7 +282,7 @@ app.post("/api/processMessage", async (_req, res) => {
       const phoneNumber = `(0${senderNum.substring(
         3,
         5
-      )}) ${senderNum.substring(5, 8)}-${senderNum.substring(9, 13)}`;
+      )}) ${senderNum.substring(5, 8)}-${senderNum.substring(8, 12)}`;
       // const phoneNumber = _req.body.senderData.sender
       //   .replace(/^972/, "0")
       //   .replace(/@c\.us$/, "");
