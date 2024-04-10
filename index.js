19cca1c216eb4dc628de957c5443680bd2d82005
moshe212 <moshe212@gmail.com>
2024-04-10 16:07:13 +0300

    add addInvited endpoint

--- index.js
+++ index.js
@@ -350,6 +350,51 @@ app.post("/api/updateTotalInvitedInEventsTable", async (_req, res) => {
     });
 });
 
+app.post("/api/addInvited", async (_req, res) => {
+  console.log("addInvited");
+  console.log("req", _req.body);
+  const {
+    FirstName,
+    LastName,
+    UserID,
+    TableID,
+    Closeness,
+    Phone,
+    AmountInvited,
+    Notes,
+    DoSendMessage,
+  } = _req.body;
+  console.log(
+    FirstName,
+    LastName,
+    UserID,
+    TableID,
+    Closeness,
+    Phone,
+    AmountInvited,
+    Notes,
+    DoSendMessage
+  );
+  res.status(200).send("ok");
+  // dbFunc
+  //   .updateTotalInvitedInEventsTable({
+  //     eventID,
+  //   })
+  //   .then((data) => {
+  //     // Assuming `data` is what the promise resolves with
+  //     console.log(`updateTotalInvitedInEventsTable: ${data}`);
+  //     // Successfully added row, send back a success response
+  //     res.status(200).send("ok");
+  //   })
+  //   .catch((error) => {
+  //     // Properly catch and handle any errors
+  //     console.error(error); // Log the error for debugging
+  //     res
+  //       .status(500)
+  //       .send("error on updateTotalInvitedInEventsTable: " + error);
+  //   });
+});
+
 app.get("/", (req, res) => {
   res.redirect("/he");
 });
