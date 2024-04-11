f65fbc513aca3eb510772b3a31c37bfc6c6ee9b7
moshe212 <moshe212@gmail.com>
2024-04-11 16:18:25 +0300

    fix EventID in addTableCaple func

--- utils/dbFunc/addTableCaple.js
+++ utils/dbFunc/addTableCaple.js
@@ -12,16 +12,31 @@ const addTableCaple = async ({ TableID, UserID, AmountSeats }) => {
   // Reference to the TableOfEvent table
   const tableOfEvent = base("TableOfEvent");
 
+  const getEventIdForUser = async (userId) => {
+    console.log("getEventIdForUser");
+    console.log("userId: " + userId);
+    const eventsTable = airtableBase("Events");
+    const records = await eventsTable
+      .select({
+        filterByFormula: `{UserID} = '${userId}'`,
+      })
+      .firstPage();
+
+    // Assuming the first matching record has the EventID you need
+    return records.length > 0 ? records[0].fields.EventID : null;
+  };
+
   try {
     // Optionally, check if a row with the same TableID and UserID already exists
     // This step is optional and depends on your application's logic
-
+    const EventID = await getEventIdForUser(UserID);
     // Prepare the row to insert
     const rowToInsert = {
       fields: {
         TableID: TableID, // Assuming TableID is a string or number
         UserID: UserID, // Assuming UserID is a string or number that references a user
         AmountSeats: AmountSeats, // Assuming AmountSeats is a number
+        EventID: EventID,
       },
     };
 
